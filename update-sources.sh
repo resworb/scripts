f9c2adc79a3f598b852edac1fbc6d92754473488
Simon Hausmann <simon.hausmann@nokia.com>
2011-11-25 17:01:00 +0100

    Avoid fetching Qt sources unless the sha1 changed.

--- update-sources.sh
+++ update-sources.sh
@@ -9,9 +9,14 @@ qt5hash=$(cat $d/qt5-pinned-hash)
 
 echo "Updating qt..."
 cd $qt5_dir
-git fetch --recurse-submodules
-git checkout $qt5hash
-git submodule update --recursive
+if [ `git rev-parse HEAD` != "$qt5hash" ]; then
+    git fetch --recurse-submodules
+    git checkout $qt5hash
+    git submodule update --recursive
+    echo "Qt sources updated. You need to rebuild Qt."
+else
+    echo "Qt sources are already up-to-date."
+fi
 cd ../..
 
 echo "Updating webkit..."
