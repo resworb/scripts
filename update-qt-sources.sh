f002d66bacd85cfc4ff8a00dbd128d8741a6fc80
Simon Hausmann <simon.hausmann@nokia.com>
2011-11-28 09:09:53 +0100

    Split out Qt updating script

---
+++ update-qt-sources.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -e
+
+d=`dirname $0`
+. $d/common.sh
+
+qt5hash=$(cat $d/qt5-pinned-hash)
+
+echo "Updating qt..."
+cd $qt5_dir
+if [ `git rev-parse HEAD` != "$qt5hash" ]; then
+    git fetch --recurse-submodules
+    git checkout $qt5hash
+    git submodule update --recursive
+    echo "Qt sources updated. You need to rebuild Qt."
+else
+    echo "Qt sources are already up-to-date."
+fi
+
--- update-sources.sh
+++ update-sources.sh
@@ -5,19 +5,7 @@ set -e
 d=`dirname $0`
 . $d/common.sh
 
-qt5hash=$(cat $d/qt5-pinned-hash)
-
-echo "Updating qt..."
-cd $qt5_dir
-if [ `git rev-parse HEAD` != "$qt5hash" ]; then
-    git fetch --recurse-submodules
-    git checkout $qt5hash
-    git submodule update --recursive
-    echo "Qt sources updated. You need to rebuild Qt."
-else
-    echo "Qt sources are already up-to-date."
-fi
-cd ../..
+$d/update-qt-sources.sh
 
 echo "Updating webkit..."
 cd $webkit_dir
